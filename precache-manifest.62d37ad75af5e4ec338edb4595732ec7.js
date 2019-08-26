5b987729c0fb84c4a6044030106db777d0ee4451
sushil11 <sushilmoktan88@gmail.com>
2019-08-26 20:12:04 +0545

    Updates

---
+++ precache-manifest.62d37ad75af5e4ec338edb4595732ec7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca51f28b427886800f7d0b6a5e0e8fb",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3632b16636e5d419e77a",
+    "url": "/monster-rolodex/static/css/main.ed67de15.chunk.css"
+  },
+  {
+    "revision": "90bc354edad42df017e0",
+    "url": "/monster-rolodex/static/js/2.08bb4953.chunk.js"
+  },
+  {
+    "revision": "3632b16636e5d419e77a",
+    "url": "/monster-rolodex/static/js/main.6a48648e.chunk.js"
+  },
+  {
+    "revision": "71d32483bf1c2d8981e2",
+    "url": "/monster-rolodex/static/js/runtime~main.eb8ef101.js"
+  }
+]);
